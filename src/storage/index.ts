22a90038bfab617812ac2dfacd621f06e897e704
hon4olo <145094796+hon4olo@users.noreply.github.com>
2026-07-22 17:51:25 +0300

    [ota] export sync cursor storage

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -3,3 +3,6 @@ export { createAsyncStorageAdapter } from './AsyncStorageAdapter';
 export { createAsyncStorageOperationQueueStore, OFFLINE_SYNC_QUEUE_STORAGE_KEY } from './AsyncStorageOperationQueueStore';
 export { createWeightSyncMetadataStore, WEIGHT_SYNC_METADATA_STORAGE_KEY } from './WeightSyncMetadataStore';
 export type { WeightSyncMetadata, WeightSyncMetadataStore } from './WeightSyncMetadataStore';
+export { createSyncCursorStore, SYNC_CURSOR_STORAGE_KEY } from './SyncCursorStore';
+export type { SyncCursor, SyncCursorStore } from './SyncCursorStore';
+export { getDefaultSyncCursorStore } from './defaultSyncCursorStore';
