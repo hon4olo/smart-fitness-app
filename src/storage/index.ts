5482e9b4018be96290ffe8d799b5b5ba29ade572
hon4olo <145094796+hon4olo@users.noreply.github.com>
2026-07-24 07:50:35 +0300

    feat: export app mutation recovery storage

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,6 +1,14 @@
 export type { StorageAdapter } from './StorageAdapter';
 export { createAsyncStorageAdapter } from './AsyncStorageAdapter';
 export { createAsyncStorageOperationQueueStore, OFFLINE_SYNC_QUEUE_STORAGE_KEY } from './AsyncStorageOperationQueueStore';
+export {
+  createAppMutationOutboxRecoveryStore,
+  APP_MUTATION_OUTBOX_RECOVERY_STORAGE_KEY,
+} from './AppMutationOutboxRecoveryStore';
+export type {
+  AppMutationOutboxRecoveryRecord,
+  AppMutationOutboxRecoveryStore,
+} from './AppMutationOutboxRecoveryStore';
 export { createWeightSyncMetadataStore, WEIGHT_SYNC_METADATA_STORAGE_KEY } from './WeightSyncMetadataStore';
 export type { WeightSyncMetadata, WeightSyncMetadataStore } from './WeightSyncMetadataStore';
 export {
