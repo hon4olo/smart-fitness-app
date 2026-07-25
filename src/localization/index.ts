e82e131fec1b6687fe7ec6a7dc5e012702ea7745
hon4olo <145094796+hon4olo@users.noreply.github.com>
2026-07-25 11:22:05 +0300

    feat: export localization contracts

---
+++ src/localization/index.ts
@@ -0,0 +1,16 @@
+export {
+  LANGUAGE_PREFERENCE_STORAGE_KEY,
+  LocalizationProvider,
+  detectSystemLocale,
+  resolveLocale,
+  translate,
+  useLocalization,
+} from './LocalizationProvider';
+export {
+  MESSAGE_CATALOGS,
+  enMessages,
+  ruMessages,
+  type LanguagePreference,
+  type MessageKey,
+  type SupportedLocale,
+} from './messages';
